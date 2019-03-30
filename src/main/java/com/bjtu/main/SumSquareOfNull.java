d582546794cca78d6b09219603e4206f40ed857b
baiyaguang <301070179@qq.com>
2019-03-30 11:25:04 +0800

    统计不含点的区域个数

---
+++ src/main/java/com/bjtu/main/SumSquareOfNull.java
@@ -0,0 +1,58 @@
+package com.bjtu.main;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import com.bjtu.model.Point;
+import com.bjtu.model.Square;
+import com.bjtu.util.CalUtil;
+import com.bjtu.util.FileUtil;
+import com.bjtu.util.SquareUtils;
+
+/**
+ * 统计空值的区域
+ * 
+ * @author apple
+ */
+public class SumSquareOfNull {
+
+    private static Logger logger = LoggerFactory.getLogger(SumSquareOfNull.class);
+    private static double len = 320.0;
+    private static double height = 160.0;
+    private static double unit = 1;
+
+    public static void main(String[] args) {
+        // 1. 首先读取点的个数
+        ArrayList<Point> pointList = (ArrayList<Point>) FileUtil.loadata();
+        logger.info(" === >>> 共读取：{} 个点", pointList.size());
+        // 2. 划分区域
+        Map<String, Square> squareMap = new HashMap<>();
+        List<Square> squareList = SquareUtils.divideSquare(len, height, unit, squareMap);
+        logger.info(" === >>> 共划分区域：{} 个", squareList.size());
+        // 3. 统计每个区域中点的个数
+        CalUtil.calPointNum(squareList, pointList);
+        logger.info(" === >>> 区域统计结束 <<< === ");
+        ArrayList<Double> countList = new ArrayList<>(); // 统计点的集合
+        // 打印 划分好的网格中 count 大于 0 的区域
+        int count = 0;
+        int index = 0;
+        for (Square square : squareList) {
+            if (square.getCount() > 0) {
+                index++;
+                logger.info("x1:" + square.getX1() + ", x2:" + square.getX2() + ", y1:" + square.getY1() + ", y2:"
+                        + square.getY2() + ", count:" + square.getCount());
+            } else {
+                count++;
+            }
+            countList.add((double) square.getCount()); // 向点的集合中添加点
+        }
+        logger.info("不包含点的区域共有：" + count);
+        logger.info("包含点的区域共有：" + index);
+
+    }
+}
